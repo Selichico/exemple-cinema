5fe88ee22a06bea0d4d25f693743f2d220c7db63
salim BENABDELLAH <salim.benabdellah@hotmail.fr>
2024-07-16 22:06:38 +0200

    Add class client

---
+++ exemple-cinema/Cinema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemple_cinema
+{
+    public class Cinema
+    {
+        public string nom;
+        public string adress;
+        public Salle salle;
+        List<Salle> salles;
+
+
+
+     }
+}
---
+++ exemple-cinema/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemple_cinema
+{
+    public class Client
+    {
+        public string nom; 
+        List<Client> clientsFidel;
+
+        public void Reservation()
+        {
+        }
+       
+    }
+}
